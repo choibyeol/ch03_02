82f8108c5582fbdf486eb366fc3df1439dc24b32
ChoiByeol <honey333888@naver.com>
2021-06-07 05:29:34 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # object가 없으면 404 에러, 있으면 get_object, 오브젝트를 가져와서 Post에 저장하는 단계.
+    # pk에 해당하는 숫자가 있으면 Post, 블로그 글에 해당하는 객체가 Post가 저장이 되고 없으면 404 에러
+    return render(request, 'blog/post_detail.html', {'post': post})
+    # 렌더링을 하는데 blog에 있는 post_detail.html 가지고 post 변수에 post를 넣어줘라.
